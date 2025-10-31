400331fcf59ada63ac18f7f89ba9466e46820970
cyrilgarcia009 <cyrilgarcia009@gmail.com>
2025-10-30 20:07:03 -0400

    Adding a logic of dataset with a temporal lookback (to replicate temporal embedding discussed)

--- tsnn/utils.py
+++ tsnn/utils.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import random_split
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, Subset
 
 
 class TorchDataset(Dataset):
@@ -18,7 +18,44 @@ class TorchDataset(Dataset):
             return self.X[idx]
 
 
-def np_to_torch(X, y=None, train_test_split=True, train_pct=0.7, batch_size=64, shuffle=True):
+class TorchDatasetRolling(Dataset):
+    def __init__(self, X, y=None, n=10):
+        self.X = X
+        self.y = y
+        self.n = n
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        start = max(0, idx - self.n + 1)
+        if self.y is not None:
+            return self.X[start:idx + 1], self.y[idx]
+        else:
+            return self.X[start:idx + 1]
+
+
+def collate_pad_beginning(batch, pad_value=0.0, max_len=None):
+    Xs, ys = zip(*batch)
+
+    max_m = max_len or max(x.shape[0] for x in Xs)
+    N, K = Xs[0].shape[1], Xs[0].shape[2]
+
+    batch_size = len(Xs)
+    X_padded = torch.full((batch_size, max_m, N, K), pad_value)
+    mask = torch.zeros((batch_size, max_m), dtype=torch.bool)
+
+    for i, x in enumerate(Xs):
+        m = x.shape[0]
+        X_padded[i, -m:] = x
+        mask[i, -m:] = True
+
+    y_batch = torch.stack(ys)
+    return X_padded, y_batch#, mask
+
+
+def np_to_torch(X, y=None, train_test_split=True, train_pct=0.7, batch_size=64, shuffle=True, n_rolling=1,
+                ts_split=True):
     """
     converts tensors to a torch dataset with option of having a train/test split
     :param X:
@@ -28,17 +65,42 @@ def np_to_torch(X, y=None, train_test_split=True, train_pct=0.7, batch_size=64, 
     :param batch_size:
     :return:
     """
-    full_data = TorchDataset(X, y)
+    if n_rolling == 1:
+        full_data = TorchDataset(X, y)
+    else:
+        full_data = TorchDatasetRolling(X, y, n=n_rolling)
     if train_test_split:
         train_size = int(train_pct * len(full_data))
         test_size = len(full_data) - train_size
-        train_data, test_data = random_split(full_data, [train_size, test_size],
-                                             generator=torch.Generator().manual_seed(42))
-        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
-        test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=shuffle)
+
+        if ts_split:
+            total_size = len(full_data)
+
+            train_indices = list(range(train_size))
+            test_indices = list(range(train_size, total_size))
+
+            train_data = Subset(full_data, train_indices)
+            test_data = Subset(full_data, test_indices)
+        else:
+            train_data, test_data = random_split(full_data, [train_size, test_size],
+                                                 generator=torch.Generator().manual_seed(42))
+
+        if n_rolling == 1:
+            train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
+            test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=shuffle)
+        else:
+            train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle,
+                                      collate_fn=collate_pad_beginning
+                                      )
+            test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=shuffle,
+                                     collate_fn=collate_pad_beginning
+                                     )
         return train_loader, test_loader
     else:
-        return DataLoader(full_data, batch_size=batch_size, shuffle=shuffle)
+        if n_rolling == 1:
+            return DataLoader(full_data, batch_size=batch_size, shuffle=shuffle)
+        else:
+            return DataLoader(full_data, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_pad_beginning)
 
 
 def torch_to_np(d):
